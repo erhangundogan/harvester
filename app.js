63981d0979cc4264457916ea5ae76c46e7817300
erhangundogan <erhan@trposta.net>
2013-04-12 20:16:03 +0300

    random timer, proxy item sparator added to settings, content control added

--- app.js
+++ app.js
@@ -1,169 +1,173 @@
-//(function harvester() {
-//  "use strict";
+var request = require("request"),
+    fs = require("fs"),
+    path = require("path"),
+    jstools = require("jstools"),
+    colors = require("colors"),
+    settings = require("./settings"),
+    Search = require("./schema/search").Search,
+    proxy_counter = 0,
+    address_counter = 0,
+    log_filename = "",
+    proxy_object = {},
+    request_counter = 0;
 
-  var request = require("request"),
-      fs = require("fs"),
-      path = require("path"),
-      jstools = require("jstools"),
-      colors = require("colors"),
-      settings = require("./settings"),
-      Search = require("./schema/search").Search,
-      proxy_counter = 0,
-      address_counter = 0,
-      log_filename = "",
-      proxy_object = {},
-      request_counter = 0;
-
-  function write_log(data) {
-    if (settings.log_to_file) {
-      fs.appendFile(path.join(__dirname, "log", log_filename), data, function (err, result) {
-        if (err) throw err.stack||err;
-      });
-    }
-  }
-
-  if (settings.proxy_from_file) {
-    if (!jstools.isNullOrEmpty(settings.proxy_filename) &&
-      fs.existsSync(path.join(__dirname, "proxy", settings.proxy_filename))) {
-        var proxy_data = fs.readFileSync(path.join(__dirname, "proxy", settings.proxy_filename), "utf-8");
-        var raw_proxy_array = proxy_data.split("\r\n").trim();
-        for (var index in raw_proxy_array) {
-          if (!jstools.isNullOrEmpty(raw_proxy_array[index])) {
-            var proxy_address = "http://"+raw_proxy_array[index]+"/";
-            if (proxy_object[proxy_address]) {
-              console.log("Multiple proxy declaration: ", proxy_address);
-            } else {
-              proxy_object[proxy_address] = {};
-              settings.proxy_list.push(proxy_address);
-            }
-          }
-        }
-      }
-  }
-
-  if (settings.address_from_file) {
-    if (!jstools.isNullOrEmpty(settings.address_filename) &&
-      fs.existsSync(path.join(__dirname, "proxy", settings.address_filename))) {
-        var address_data = fs.readFileSync(path.join(__dirname, "address", settings.address_filename));
-        settings.address_list = address_data.split("\r\n").trim();
-      }
-  }
-
+function write_log(data) {
   if (settings.log_to_file) {
-    var dt = new Date();
-    log_filename = dt.getFullYear()+""+
-                   ("0"+(dt.getMonth()+1)).slice(-2)+""+
-                   ("0"+dt.getDate()).slice(-2)+"-"+
-                   dt.getTime()+".log";
+    fs.appendFile(path.join(__dirname, "log", log_filename), data, function (err, result) {
+      if (err) throw err.stack||err;
+    });
   }
-  
-  var interval = setInterval(function(){
-    if (address_counter >= settings.address_list.length) {
-      var control_interval = setInterval(function(){
-        if (request_counter <= 0) {
-          var message = JSON.stringify(proxy_object);
-          console.log(message);
-          if (settings.log_to_file) write_log(message);
-          process.exit(1);
-        }
-      }, 1000);
-    } else {
-      ++request_counter;
-      var proxy_address = settings.proxy_list[proxy_counter];
-      proxy_object[proxy_address].begin_time = new Date();
-      proxy_object[proxy_address].address = settings.address_list[address_counter];
-      proxy_object[proxy_address].method = settings.request_method;
-      console.log(proxy_address, "\t\t=REQUEST=>\t\t".cyan, settings.address_list[address_counter]);
+}
 
-      request({
-        "method": settings.request_method,
-        "uri": settings.address_list[address_counter],
-        "headers": settings.request_headers,
-        "proxy": proxy_address,
-        "timeout": settings.request_timeout
-      },
-        function (err, res, body) {
-          --request_counter;
-          var now = new Date(),
-              result_proxy = this.proxy.href,
-              result_address = this.href,
-              current_proxy = proxy_object[result_proxy],
-              duration = (now-current_proxy.begin_time)/1000;
-
-          console.log(result_proxy, (err?"\t\t<=ERROR=\t\t".red:"\t\t<=SUCCESS=\t\t".green), result_address, "\t\t", duration, " secs.");
-          if (err) {
-            if (settings.log_to_mongodb) {
-              var search = new Search();
-              if (res && res.statusCode) search.status_code = res.statusCode;
-              if (res && res.body) search.body_length = res.body.length;
-              if (res && res.headers) search.headers = res.headers;
-              if (this.redirects) search.redirects = this.redirects;
-              search.return_address = result_address;
-              search.proxy = result_proxy;
-              search.address = current_proxy.address;
-              search.time = current_proxy.begin_time;
-              search.duration = duration;
-              search.success = false;
-              search.method = settings.request_method;
-              search.error = err.stack||err;
-              search.save(function(err) {
-                if(err) {
-                  console.log(err.stack||err);
-                  write_log(err.stack||err);
-                }
-              });
-            }
-
-            if (settings.log_to_file) {
-              if (res && res.statusCode) current_proxy.status = res.statusCode;
-              if (res && res.body) current_proxy.body_length = res.body.length;
-              if (res && res.headers) current_proxy.headers = res.headers;
-              if (this.redirects) current_proxy.redirects = this.redirects;
-              current_proxy.duration = duration;
-              current_proxy.success = false;
-              current_proxy.error = err.stack||err;
-              current_proxy.return_address = result_address;
-            }
+if (settings.proxy_from_file) {
+  if (!jstools.isNullOrEmpty(settings.proxy_filename) &&
+    fs.existsSync(path.join(__dirname, "proxy", settings.proxy_filename))) {
+      var proxy_data = fs.readFileSync(path.join(__dirname, "proxy", settings.proxy_filename), "utf-8");
+      var raw_proxy_array = proxy_data.split(settings.proxy_file_separator).trim();
+      for (var index in raw_proxy_array) {
+        if (!jstools.isNullOrEmpty(raw_proxy_array[index])) {
+          var proxy_address = "http://"+raw_proxy_array[index]+"/";
+          if (proxy_object[proxy_address]) {
+            console.log("Multiple proxy declaration: ", proxy_address);
           } else {
-            if (settings.log_to_mongodb) {
-              var search = new Search();
-              if (res && res.statusCode) search.status_code = res.statusCode;
-              if (res && res.body) search.body_length = res.body.length;
-              if (res && res.headers) search.headers = res.headers;
-              if (this.redirects) search.redirects = this.redirects;
-              search.return_address = result_address;
-              search.proxy = result_proxy;
-              search.address = current_proxy.address;
-              search.time = current_proxy.begin_time;
-              search.duration = duration;
-              search.method = settings.request_method;
-              search.success = true;
-              search.save(function(err) {
-                if(err) {
-                  console.log(err.stack||err);
-                  write_log(err.stack||err);
-                }
-              });
-            }
-
-            if (settings.log_to_file) {
-              if (res && res.statusCode) current_proxy.status = res.statusCode;
-              if (res && res.body) current_proxy.body_length = res.body.length;
-              if (res && res.headers) current_proxy.headers = res.headers;
-              if (this.redirects) current_proxy.redirects = this.redirects;
-              current_proxy.duration = duration;
-              current_proxy.success = true;
-              current_proxy.return_address = result_address;
-            }
+            proxy_object[proxy_address] = {};
+            settings.proxy_list.push(proxy_address);
           }
         }
-      );
-
-      if (++proxy_counter === settings.proxy_list.length) {
-        proxy_counter = 0;
-        ++address_counter;
       }
     }
-  }, settings.timer);
-//})();
+}
 
+if (settings.address_from_file) {
+  if (!jstools.isNullOrEmpty(settings.address_filename) &&
+    fs.existsSync(path.join(__dirname, "proxy", settings.address_filename))) {
+      var address_data = fs.readFileSync(path.join(__dirname, "address", settings.address_filename));
+      settings.address_list = address_data.split("\r\n").trim();
+    }
+}
+
+if (settings.log_to_file) {
+  var dt = new Date();
+  log_filename = dt.getFullYear()+""+
+                 ("0"+(dt.getMonth()+1)).slice(-2)+""+
+                 ("0"+dt.getDate()).slice(-2)+"-"+
+                 dt.getTime()+".log";
+}
+
+var interval = setInterval(function(){
+  if (address_counter >= settings.address_list.length) {
+    var control_interval = setInterval(function(){
+      if (request_counter <= 0) {
+        var message = JSON.stringify(proxy_object);
+        console.log(message);
+        if (settings.log_to_file) write_log(message);
+        process.exit(1);
+      }
+    }, 1000);
+  } else {
+    ++request_counter;
+    var proxy_address = settings.proxy_list[proxy_counter];
+    proxy_object[proxy_address].begin_time = new Date();
+    proxy_object[proxy_address].address = settings.address_list[address_counter];
+    proxy_object[proxy_address].method = settings.request_method;
+    console.log(proxy_address, "\t\t=REQUEST=>\t\t".cyan, settings.address_list[address_counter]);
+
+    request({
+      "method": settings.request_method,
+      "uri": settings.address_list[address_counter],
+      "headers": settings.request_headers,
+      "proxy": proxy_address,
+      "timeout": settings.request_timeout
+    },
+      function (err, res, body) {
+        --request_counter;
+        var now = new Date(),
+            result_proxy = this.proxy.href,
+            result_address = this.href,
+            current_proxy = proxy_object[result_proxy],
+            duration = (now-current_proxy.begin_time)/1000,
+            expectedResult = true;
+
+        if (!err && settings.request_method === "GET" && !jstools.isNullOrEmpty(settings.body_includes)) {
+          var regex = new RegExp(settings.body_includes);
+          expectedResult = regex.test(body);
+        }
+
+        if (err || !expectedResult) {
+          console.log(result_proxy, "\t\t<=ERROR=\t\t".red, result_address, "\t\t", duration, " secs.");
+          if (settings.log_to_mongodb) {
+            var search = new Search();
+            if (res && res.statusCode) search.status_code = res.statusCode;
+            if (res && res.body) search.body_length = res.body.length;
+            if (res && res.headers) search.headers = res.headers;
+            if (this.redirects) search.redirects = this.redirects;
+            search.return_address = result_address;
+            search.proxy = result_proxy;
+            search.address = current_proxy.address;
+            search.time = current_proxy.begin_time;
+            search.duration = duration;
+            search.success = false;
+            search.method = settings.request_method;
+            search.error = err ? err.stack||err : "Not expected content returned";
+            search.save(function(err) {
+              if(err) {
+                console.log(err.stack||err);
+                write_log(err.stack||err);
+              }
+            });
+          }
+
+          if (settings.log_to_file) {
+            if (res && res.statusCode) current_proxy.status = res.statusCode;
+            if (res && res.body) current_proxy.body_length = res.body.length;
+            if (res && res.headers) current_proxy.headers = res.headers;
+            if (this.redirects) current_proxy.redirects = this.redirects;
+            current_proxy.duration = duration;
+            current_proxy.success = false;
+            current_proxy.error = err ? err.stack||err : "Not expected content returned";
+            current_proxy.return_address = result_address;
+          }
+        } else {
+          console.log(result_proxy, "\t\t<=SUCCESS=\t\t".green, result_address, "\t\t", duration, " secs.");
+          if (settings.log_to_mongodb) {
+            var search = new Search();
+            if (res && res.statusCode) search.status_code = res.statusCode;
+            if (res && res.body) search.body_length = res.body.length;
+            if (res && res.headers) search.headers = res.headers;
+            if (this.redirects) search.redirects = this.redirects;
+            search.return_address = result_address;
+            search.proxy = result_proxy;
+            search.address = current_proxy.address;
+            search.time = current_proxy.begin_time;
+            search.duration = duration;
+            search.method = settings.request_method;
+            search.success = true;
+            search.save(function(err) {
+              if(err) {
+                console.log(err.stack||err);
+                write_log(err.stack||err);
+              }
+            });
+          }
+
+          if (settings.log_to_file) {
+            if (res && res.statusCode) current_proxy.status = res.statusCode;
+            if (res && res.body) current_proxy.body_length = res.body.length;
+            if (res && res.headers) current_proxy.headers = res.headers;
+            if (this.redirects) current_proxy.redirects = this.redirects;
+            current_proxy.duration = duration;
+            current_proxy.success = true;
+            current_proxy.return_address = result_address;
+          }
+        }
+      }
+    );
+
+    if (++proxy_counter === settings.proxy_list.length) {
+      proxy_counter = 0;
+      ++address_counter;
+    }
+  }
+}, settings.random_timer
+   ? (Math.floor(Math.random() * settings.timer_add_random) + settings.timer_min) * 1000
+   : settings.timer * 1000);
--- settings.js
+++ settings.js
@@ -1,18 +1,23 @@
 module.exports = {
-  "address_list": ["http://www.microsoft.com", "http://www.google.com"], // string array of url adddresses
+  "address_list": ["http://www.wired.com"], // string array of url adddresses
   "proxy_list": [], // string array of proxies e.g. 0.0.0.0:8080
-  "timer": 5000, // request delay for each one
+  "timer": 10, // seconds request delay for each one if no random timer
+  "random_timer": true,
+  "timer_min": 5, // seconds min time
+  "timer_add_random": 30, // seconds min time + add random time
   "log_to_file" : false, // log results to a file
   "log_to_mongodb" : true, // log results to a mongodb database
   "mongodb_connection" : "mongodb://localhost/harvester", // mongodb database connection
-  "mongodb_collection" : "search", // mongodb collection to save
+  "mongodb_collection" : "proxy_result", // mongodb collection to save
   "proxy_from_file" : true, // if it is true, reads proxies list from file specified in :proxy_filename
+  "proxy_file_separator" : "\n", // proxy items separator
   "address_from_file" : false, // if it is true, reads addresses list from file specified in :address_filename
   "proxy_filename" : "proxylist.txt", // this file should be put under proxy directory, do not specify directory name
   "address_filename" : "", // this file should be put under address directory, do not specify directory name
   "request_method" : "GET", // default request method, do not change this one. GET would be implemented later
   "request_timeout" : 180000, // Integer containing the number of milliseconds to wait for a request to respond before aborting the request
-  "request_headers" : {
+  "body_includes" : "<title>wired.com", // use some text from webpage if you want to test results, use GET for method
+  "request_headers_chrome" : {
     // specify your own properties if you want
     "Accept": "text/html",
     "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
@@ -20,5 +25,13 @@ module.exports = {
     "Accept-Language": "en-US,en;q=0.8",
     "Connection": "keep-alive",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.40 Safari/537.31"
+  },
+  "request_headers" : {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0",
+    "Connection": "keep-alive",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "deflate",
+    "Accept-Charset": "utf-8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
   }
 };
